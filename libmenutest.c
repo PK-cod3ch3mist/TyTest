90ae194b00f908fffaf3123dd66606b44a607d3c
Pratyush Kumar <pratyushkumartheking@gmail.com>
2022-03-23 13:19:04 +0530

    Made code modular

--- libmenutest.c
+++ libmenutest.c
@@ -1,7 +1,5 @@
-#include <curses.h>
-#include <menu.h>
-#include <stdlib.h>
-#include <string.h>
+#include "window-util.h"
+#include <time.h>
 
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
 
@@ -27,64 +25,23 @@ int main() {
     cbreak();
     noecho();
     keypad(stdscr, TRUE);
-    init_pair(1, COLOR_BLUE, COLOR_BLACK);
+    init_pair(1, COLOR_CYAN, COLOR_BLACK);
     init_pair(2, COLOR_WHITE, COLOR_BLACK);
-    init_pair(3, COLOR_WHITE, COLOR_RED);
+    init_pair(3, COLOR_RED, COLOR_BLACK);
+    init_pair(4, COLOR_GREEN, COLOR_BLACK);
 
-    int curr_choice = main_menu();
+    int curr_choice = 0;
+    do {
+        curr_choice = main_menu();
 
-    if (curr_choice == 0) {
-        test_func();
-    }
-    else if (curr_choice == 1) {
-        about_func();
-    }
+        if (curr_choice == 0) {
+            test_func();
+        }
+        else if (curr_choice == 1) {
+            about_func();
+        }
+    } while(curr_choice != 2);
     endwin();
-}
-
-void print_in_middle(WINDOW *win, int starty, int startx, int width, char *string, chtype color) {
-    int length, x, y;
-    float temp;
-
-    if (win == NULL)
-        win = stdscr;
-    getyx(win, y, x);
-    if (startx != 0)
-        x = startx;
-    if (starty != 0)
-        y = starty;
-    if (width == 0)
-        width = 80;
-
-    length = strlen(string);
-    temp = (width - length)/2;
-    x = startx + (int)temp;
-    wattron(win, color);
-    mvwprintw(win, y, x, "%s", string);
-    wattroff(win, color);
-    refresh();
-}
-
-void destroy_win(WINDOW *local_win) {
-    wclear(local_win);
-    /* box(local_win, ' ', ' '); : This won't produce the desired
-     * result of erasing the window. It will leave it's four corners
-     * and so an ugly remnant of window.
-     */
-    wborder(local_win, ' ', ' ', ' ',' ',' ',' ',' ',' ');
-    /* The parameters taken are
-     * 1. win: the window on which to operate
-     * 2. ls: character to be used for the left side of the window
-     * 3. rs: character to be used for the right side of the window
-     * 4. ts: character to be used for the top side of the window
-     * 5. bs: character to be used for the bottom side of the window
-     * 6. tl: character to be used for the top left corner of the window
-     * 7. tr: character to be used for the top right corner of the window
-     * 8. bl: character to be used for the bottom left corner of the window
-     * 9. br: character to be used for the bottom right corner of the window
-     */
-    wrefresh(local_win);
-    delwin(local_win);
 }
 
 void test_func() {
@@ -92,42 +49,32 @@ void test_func() {
     WINDOW *wbw_win;
     int starty = (LINES - 20) / 2;
     int startx = (COLS - 100) / 2;
-    wbw_win = newwin(20, 100, starty, startx);
-    wborder(wbw_win, '|', '|', '-', '-', '+', '+', '+', '+');
-    wattron(wbw_win, A_BOLD);
-    print_in_middle(wbw_win, 1, 0, 100, "Word by Word Test", COLOR_PAIR(1));
-    wattroff(wbw_win, A_BOLD);
-    mvwaddch(wbw_win, 2, 0, '+');
-    mvwhline(wbw_win, 2, 1, '-', 98);
-    mvwaddch(wbw_win, 2, 99, '+');
+    wbw_win = create_window(startx, starty, 20, 100, "No Error Allowed Test", COLOR_PAIR(1));
 
     c = 0;
 
     do {
-        print_in_middle(wbw_win, 3, 0, 100, "In this test, you will be required to type word by word.", COLOR_PAIR(2));
-        print_in_middle(wbw_win, 4, 0, 100, "In the end you will be given an error score as well.", COLOR_PAIR(2));
-        print_in_middle(wbw_win, 5, 0, 100, "You will not be able to see the whole text though, only the current word highlighted.", COLOR_PAIR(2));
-        print_in_middle(wbw_win, 6, 0, 100, "The test will start as soon you press the key.", COLOR_PAIR(2));
-        wattron(wbw_win, A_BLINK);
-        print_in_middle(wbw_win, 17, 0, 100, "Press Enter to start", COLOR_PAIR(3));
-        wattroff(wbw_win, A_BLINK);
+        print_in_middle_para(wbw_win, 5, 15, 70, "In this test, you will be required to type word by word. In the end you will be given an error score as well. You will not be able to see the whole text though, only the current word highlighted. The test will start as soon you press the key.", COLOR_PAIR(2));
+        wattron(wbw_win, A_BOLD);
+        print_in_middle(wbw_win, 16, 0, 100, "Press Enter to start", COLOR_PAIR(4));
+        print_in_middle(wbw_win, 17, 0, 100, "Enter character 'x' to exit", COLOR_PAIR(3));
+        wattroff(wbw_win, A_BOLD);
         wrefresh(wbw_win);
         if (c == 10) break; /* Enter key is pressed */
+        else if (c == (int)'x') {
+            destroy_win(wbw_win);
+            return;
+        }
     } while((c = wgetch(wbw_win)));
 
-    wclear(wbw_win);
-    wborder(wbw_win, '|', '|', '-', '-', '+', '+', '+', '+');
-    wattron(wbw_win, A_BOLD);
-    print_in_middle(wbw_win, 1, 0, 100, "Word by Word Test", COLOR_PAIR(1));
-    wattroff(wbw_win, A_BOLD);
-    mvwaddch(wbw_win, 2, 0, '+');
-    mvwhline(wbw_win, 2, 1, '-', 98);
-    mvwaddch(wbw_win, 2, 99, '+');
+    renew_win(wbw_win, 20, 100, "No Error Allowed Test", COLOR_PAIR(1));
     mvwhline(wbw_win, 17, 2, '_', 97);
     wrefresh(wbw_win);
 
-    FILE *wbwfile = fopen("wbw.txt", "r");
-    int inaccuracy = 0;
+    FILE *wbwfile = fopen("wbw-short.txt", "r");
+    int count = 0, check, loopExit = 0;
+    long double tm = 0;
+    time_t tic = (time_t)0;
 
     if (wbwfile == NULL) {
         print_in_middle(wbw_win, 3, 0, 100, "FILE FAILED TO OPEN", COLOR_PAIR(3));
@@ -136,35 +83,41 @@ void test_func() {
         echo();
         char word[100];
         char user_input[100] = "\0";
+        char message[100] = "\0";
         int index;
 
         while (fscanf(wbwfile, "%99s", word) == 1) {
             print_in_middle(wbw_win, 4, 0, 100, word, COLOR_PAIR(2));
+            sprintf(message, "Time taken in previous word : %.2lf", (double)tic);
+            print_in_middle(wbw_win, 8, 0, 100, message, COLOR_PAIR(4));
+            print_in_middle(wbw_win, 9, 0, 100, "Type quit() at any time to quit", COLOR_PAIR(3));
             wrefresh(wbw_win);
-            mvwgetstr(wbw_win, 16, 2, user_input);
-            for (index = 0; word[index] != '\0'; index++) {
-                if (word[index] != user_input[index]) inaccuracy++;
-            }
+            tic = time(NULL);
+            do {
+                check = 1;
+                mvwgetstr(wbw_win, 16, 2, user_input);
+                index = strlen(word);
+                check = (strcmp(word, user_input)) ? 0 : 1;
+                loopExit = (strcmp(user_input, "quit()")) ? 0 : 1;
+                mvwprintw(wbw_win, 16, 2, "                         ");
+            } while (!check && !loopExit);
+            if (loopExit) break;
+            tic = time(NULL) - tic;
             print_in_middle(wbw_win, 4, 0, 100, "                         ", COLOR_PAIR(2));
-            mvwprintw(wbw_win, 16, 2, "                         ");
             strcpy(word, "\0");
             strcpy(user_input, "\0");
+            count += index;
+            tm = tm + (long double)(tic);
         }
     }
     noecho();
 
-    wclear(wbw_win);
-    wborder(wbw_win, '|', '|', '-', '-', '+', '+', '+', '+');
-    wattron(wbw_win, A_BOLD);
-    print_in_middle(wbw_win, 1, 0, 100, "Word by Word Test", COLOR_PAIR(1));
-    wattroff(wbw_win, A_BOLD);
-    mvwaddch(wbw_win, 2, 0, '+');
-    mvwhline(wbw_win, 2, 1, '-', 98);
-    mvwaddch(wbw_win, 2, 99, '+');
+    renew_win(wbw_win, 20, 100, "No Error Allowed Test", COLOR_PAIR(1));
     mvwhline(wbw_win, 17, 2, '_', 97);
     wrefresh(wbw_win);
 
-    mvwprintw(wbw_win, 6, 2, "Accuracy : %f", (float)((float)(499 - inaccuracy)/499)*100);
+    mvwprintw(wbw_win, 6, 2, "Time Taken : %Lf seconds", tm);
+    mvwprintw(wbw_win, 7, 2, "Speed : %Lf characters per second", (long double)count / tm);
     print_in_middle(wbw_win, 16, 0, 100, "Press any key to exit", COLOR_PAIR(3));
     c = wgetch(wbw_win);
 
@@ -199,7 +152,7 @@ int main_menu() {
     set_menu_win(my_menu, my_menu_win);
     set_menu_sub(my_menu, derwin(my_menu_win, 6, 58, 3, 1));
 
-    /* Set menu mark to the string " * " */
+    /* Set menu mark to the string " > " */
     set_menu_mark(my_menu, " > ");
 
     /* Print a border around the main window and print a title */
@@ -251,20 +204,11 @@ void about_func() {
     WINDOW *about_win;
     int starty = (LINES - 20) / 2;
     int startx = (COLS - 100) / 2;
-    about_win = newwin(20, 100, starty, startx);
-    wborder(about_win, '|', '|', '-', '-', '+', '+', '+', '+');
-    wattron(about_win, A_BOLD);
-    print_in_middle(about_win, 1, 0, 100, "About TyTest", COLOR_PAIR(1));
-    wattroff(about_win, A_BOLD);
-    mvwaddch(about_win, 2, 0, '+');
-    mvwhline(about_win, 2, 1, '-', 98);
-    mvwaddch(about_win, 2, 99, '+');
+    about_win = create_window(startx, starty, 20, 100, "About TyTest", COLOR_PAIR(1));
 
-    print_in_middle(about_win, 4, 0, 100, "TyTest (v0.1.0) is a terminal based typing test application.", COLOR_PAIR(2));
-    print_in_middle(about_win, 5, 0, 100, "It is made as a hobbyist project to demonstrate use of ncurses library in C.", COLOR_PAIR(2));
-    print_in_middle(about_win, 6, 0, 100, "It currently supports only a word by word test.", COLOR_PAIR(2));
-    print_in_middle(about_win, 7, 0, 100, "I plan to add different types of tests to it in the future, stay tuned!", COLOR_PAIR(2));
+    print_in_middle_para(about_win, 4, 0, 100, "TyTest (v0.1.0) is a terminal based typing test application. It is made as a hobbyist project to demonstrate use of ncurses library in C. It currently supports only a word by word test. I plan to add different types of tests to it in the future, stay tuned!", COLOR_PAIR(2));
     print_in_middle(about_win, 17, 0, 100, "Press any key to exit", COLOR_PAIR(3));
 
     c = wgetch(about_win);
+    destroy_win(about_win);
 }
---
+++ window-util.h
@@ -0,0 +1,117 @@
+#ifndef WINDOW_UTIL_H
+#define WINDOW_UTIL_H
+
+#include <curses.h>
+#include <menu.h>
+#include <string.h>
+#include <stdlib.h>
+
+void print_in_middle_para(WINDOW* win, int sy, int sx, int width, char* msg, chtype color) {
+    int len, x, y;
+    float temp;
+    int i, c = 0;
+    int printLen = 0;
+    int nextStart = 0;
+
+    if (win == NULL) {
+        win = stdscr;
+    }
+    getyx(win, y, x);
+    if (sx) x = sx;
+    if (sy) y = sy;
+    if (!width) width = 80;
+
+    len = strlen(msg);
+    wattron(win, color);
+    for (int i = 0; i < len; i++) {
+        c++;
+        if (msg[i] == ' ') {
+            printLen = i - nextStart;
+        }
+        if (c == (width - 4)) {
+            // on this line, print only till prev_space
+            temp = (width - printLen)/2;
+            x = sx + (int)temp;
+            mvwprintw(win, y++, x, "%.*s", printLen, msg + nextStart);
+            nextStart += printLen + 1;
+            c = 0;
+        }
+    }
+    temp = (width - (len - nextStart))/2;
+    x = sx + (int)temp;
+    mvwprintw(win, y, x, "%s", msg + nextStart);
+    wattroff(win, color);
+    wrefresh(win);
+}
+
+void print_in_middle(WINDOW* win, int starty, int startx, int width, char* msg, chtype color) {
+    int length, x, y;
+    float temp;
+
+    if (win == NULL)
+        win = stdscr;
+    getyx(win, y, x);
+    if (startx)
+        x = startx;
+    if (starty)
+        y = starty;
+    if (width == 0)
+        width = 80;
+
+    length = strlen(msg);
+    temp = (width - length)/2;
+    x = startx + (int)temp;
+    wattron(win, color);
+    mvwprintw(win, y, x, "%s", msg);
+    wattroff(win, color);
+    refresh();
+}
+
+WINDOW* create_window(int startx, int starty, int rows, int cols, char *title, chtype color) {
+    WINDOW *win;
+    win = newwin(rows, cols, starty, startx);
+    wborder(win, '|', '|', '-', '-', '+', '+', '+', '+');
+    wattron(win, A_BOLD);
+    print_in_middle(win, 1, 0, cols, title, color);
+    wattroff(win, A_BOLD);
+    mvwaddch(win, 2, 0, '+');
+    mvwhline(win, 2, 1, '-', cols - 2);
+    mvwaddch(win, 2, cols - 1, '+');
+    wrefresh(win);
+    return win;
+}
+
+void renew_win(WINDOW* win, int rows, int cols, char *title, chtype color) {
+    wclear(win);
+    wborder(win, '|', '|', '-', '-', '+', '+', '+', '+');
+    wattron(win, A_BOLD);
+    print_in_middle(win, 1, 0, cols, title, color);
+    wattroff(win, A_BOLD);
+    mvwaddch(win, 2, 0, '+');
+    mvwhline(win, 2, 1, '-', cols - 2);
+    mvwaddch(win, 2, cols - 1, '+');
+    wrefresh(win);
+}
+
+void destroy_win(WINDOW *local_win) {
+    wclear(local_win);
+    /* box(local_win, ' ', ' '); : This won't produce the desired
+     * result of erasing the window. It will leave it's four corners
+     * and so an ugly remnant of window.
+     */
+    wborder(local_win, ' ', ' ', ' ',' ',' ',' ',' ',' ');
+    /* The parameters taken are
+     * 1. win: the window on which to operate
+     * 2. ls: character to be used for the left side of the window
+     * 3. rs: character to be used for the right side of the window
+     * 4. ts: character to be used for the top side of the window
+     * 5. bs: character to be used for the bottom side of the window
+     * 6. tl: character to be used for the top left corner of the window
+     * 7. tr: character to be used for the top right corner of the window
+     * 8. bl: character to be used for the bottom left corner of the window
+     * 9. br: character to be used for the bottom right corner of the window
+     */
+    wrefresh(local_win);
+    delwin(local_win);
+}
+#endif
